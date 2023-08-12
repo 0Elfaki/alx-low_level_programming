5c101e0481a6757b1b24db10dc095013a1cee728
megdad <megdad.elfaki@gmail.com>
2023-08-11 21:01:36 -0400

    task 0x01 in c langauge learning path

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,6 +1,4 @@
-#include <stdio.h>
 #include <stdlib.h>
-
 /**
  * main - main block
  * Description: Write a program that prints all possible
@@ -19,38 +17,37 @@
  */
 int main(void)
 {
-    int c;
-    int d;
-    int e = 0;
+	int c;
+	int d;
+	int e = 0;
 
-    while (e < 10)
-    {
-        d = 0;
-        while (d < 10)
-        {
-            c = 0;
-            while (c < 10)
-            {
-                if (c != d && d != e && e < d && d < c)
-                {
-                    putchar('0' + e);
-                    putchar('0' + d);
-                    putchar('0' + c);
+	while (e < 10)
+	{
+		d = 0;
+		while (d < 10)
+		{
+			c = 0;
+			while (c < 10)
+			{
+				if (c != d && d != e && e < d && d < c)
+				{
+					putchar('0' + e);
+					putchar('0' + d);
+					putchar('0' + c);
 
-                    if (c + d + e != 9 + 8 + 7)
-                    {
-                        putchar(',');
-                        putchar(' ');
-                    }
-                }
+					if (c + d + e != 9 + 8 + 7)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
 
-                c++;
-            }
-            d++;
-        }
-        e++;
-    }
-    putchar('\n');
-    return (0);
-}
-          
+				c++;
+			}
+			d++;
+		}
+		e++;
+	}
+	putchar('\n');
+	return (0);
+}          
