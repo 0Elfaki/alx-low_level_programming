4629002da305edd37143d72632b48367df2e95ea
megdad <megdad.elfaki@gmail.com>
2023-08-15 11:06:08 -0400

    0x02. C - Functions, nested loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,10 @@
 #include "main.h"
 
 /**
- * main - Entry point
- * Return:0
+ * main - main block
+ * Description: Get a random number and print the number
+ * and if it is positive, negative, or zero
+ * Return: 0
  */
 
 int main(void)
