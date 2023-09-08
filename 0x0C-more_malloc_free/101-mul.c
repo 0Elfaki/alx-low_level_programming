496d182fa70f233accafb1eeccf3105571076f35
megdad <megdad.elfaki@gmail.com>
2023-09-07 20:04:23 -0400

    update

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -5,34 +5,35 @@
 
 /**
  * main - multiplies two positive numbers
- * @argc: n arguments
- * @argv: args
+ * @argc: number of arguments
+ * @argv: array of argument strings
  * Return: int
 **/
 
 int main(int argc, char *argv[])
 {
-	unsigned long mul;
-	int i, j;
+    unsigned long mul;
+    int i, j;
 
-	if (argc != 3)
-	{
-		printf("Error\n");
-		exit(98);
-	}
+    if (argc != 3)
+    {
+        printf("Error\n");
+        exit(98);
+    }
 
-	for (i = 1; i < argc; i++)
-	{
-		for (j = 0; argv[i][j] != '\0'; j++)
-		{
-			if (argv[i][j] > 57 || argv[i][j] < 48)
-			{
-				printf("Error\n");
-				exit(98);
-			}
-		}
-	}
-	mul = atol(argv[1]) * atol(argv[2]);
-	printf("%lu\n", mul);
-	return (0);
+    for (i = 1; i < argc; i++)
+    {
+        for (j = 0; argv[i][j] != '\0'; j++)
+        {
+            if (argv[i][j] < '0' || argv[i][j] > '9') /* Fix the condition here */
+            {
+                printf("Error\n");
+                exit(98);
+            }
+        }
+    }
+    mul = atol(argv[1]) * atol(argv[2]);
+    printf("%lu\n", mul);
+    return (0);
 }
+
